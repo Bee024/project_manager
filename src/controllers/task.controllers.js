00130d73115691d063554d6784f687e4ed620b58
Biswajyoti Pradhan <pradhanbiswajyoti024@gmail.com>
2026-04-24 23:30:39 +0530

    no changes

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,44 @@
+import { asyncHandler } from "../utils/async-handler.js";
+
+const getTasks = asyncHandler(async (req, res) => {
+  //chai
+});
+
+const createTask = asyncHandler(async (req, res) => {
+  //chai
+});
+
+const getTaskById = asyncHandler(async (req, res) => {
+  // chai
+});
+
+const updateTask = asyncHandler(async (req, res) => {
+  // chai
+});
+
+const deleteTask = asyncHandler(async (req, res) => {
+  // chai
+});
+
+const createSubTask = asyncHandler(async (req, res) => {
+  // chai
+});
+
+const updateSubTask = asyncHandler(async (req, res) => {
+  // chai
+});
+
+const deleteSubTask = asyncHandler(async (req, res) => {
+  // chai
+});
+
+export {
+  createSubTask,
+  createTask,
+  deleteSubTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateSubTask,
+  updateTask,
+};
