c7445d73efed84844f867f9c5059ea051b69c30b
Biswajyoti Pradhan <pradhanbiswajyoti024@gmail.com>
2026-04-26 23:44:03 +0530

    added task controllers - createTask

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,3 +1,8 @@
+import mongoose from "mongoose";
+import { Project } from "../models/project.models.js";
+import { Task } from "../models/task.models.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
 
 const getTasks = asyncHandler(async (req, res) => {
@@ -5,7 +10,37 @@ const getTasks = asyncHandler(async (req, res) => {
 });
 
 const createTask = asyncHandler(async (req, res) => {
-  //chai
+  const { title, description, assignedTo, status } = req.body;
+  const { projectId } = req.params;
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(404, " Project not found ");
+  }
+  const files = req.files || [];
+
+  const attachments = files.map((file) => {
+    return {
+      url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+      mimetype: file.mimetype,
+      size: file.size,
+    };
+  });
+
+  const task = await Task.create({
+    title,
+    description,
+    project: new mongoose.Types.ObjectId(projectId),
+    assignedTo: assignedTo
+      ? new mongoose.Types.ObjectId(assignedTo)
+      : undefined,
+    status,
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachments,
+  });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created successfully"));
 });
 
 const getTaskById = asyncHandler(async (req, res) => {
